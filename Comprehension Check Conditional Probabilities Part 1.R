7b466737217fa5cf52b9e54794408a4e5fe4545d
Sonick Suri <sonicksuri@gmail.com>
2022-06-27 21:17:15 +0530

    Assignment Conditional Prob

---
+++ Comprehension Check Conditional Probabilities Part 1.R
@@ -0,0 +1,53 @@
+
+set.seed(1, sample.kind = "Rounding") # if using R 3.6 or later
+disease <- sample(c(0,1), size=1e6, replace=TRUE, prob=c(0.98,0.02))
+test <- rep(NA, 1e6)
+test[disease==0] <- sample(c(0,1), size=sum(disease==0), replace=TRUE, prob=c(0.90,0.10))
+test[disease==1] <- sample(c(0,1), size=sum(disease==1), replace=TRUE, prob=c(0.15, 0.85))
+
+
+# Q2
+mean(test == 1)
+
+# Q3
+mean(test == 0 & disease == 1)
+
+# Q4
+mean(disease[test == 1])
+
+
+#Q5
+mean(disease[test==1]==1)/mean(disease==1)
+
+
+# Q6
+library(dslabs)
+library(tidyverse)
+data("heights")
+heights %>% 
+  mutate(height = round(height)) %>%
+  group_by(height) %>%
+  summarize(p = mean(sex == "Male")) %>%
+qplot(height, p, data =.)
+
+# Q7
+ps <- seq(0, 1, 0.1)
+heights %>% 
+  mutate(g = cut(height, quantile(height, ps), include.lowest = TRUE)) %>%
+  group_by(g) %>%
+  summarize(p = mean(sex == "Male"), height = mean(height)) %>%
+  qplot(height, p, data =.)
+
+# Q8
+Sigma <- 9*matrix(c(1,0.5,0.5,1), 2, 2)
+dat <- MASS::mvrnorm(n = 10000, c(69, 69), Sigma) %>%
+  data.frame() %>% setNames(c("x", "y"))
+
+
+# Q9
+ps <- seq(0, 1, 0.1)
+dat %>% 
+  mutate(g = cut(x, quantile(x, ps), include.lowest = TRUE)) %>%
+  group_by(g) %>%
+  summarize(y = mean(y), x = mean(x)) %>%
+  qplot(x, y, data =.)
